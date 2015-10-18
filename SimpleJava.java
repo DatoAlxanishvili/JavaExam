ddbaa5c99e92407d0f311ca6309622461e141249
Davit Alkhanishvili <davit.alkhanishvili02@geolab.edu.ge>
2015-10-18 17:06:21 +0400

    ამოცანა 4

---
+++ SimpleJava.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/**
+ * Created by GeoLabOwl on 18.10.15.
+ */
+public class SimpleJava {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num=scanner.nextInt();
+
+        int previousNumber=0;
+        int count=0;
+        int countMax=0;
+        while(num!=0){
+           if(num>previousNumber) count++;
+            if(num<previousNumber) {
+                count=1;
+            }
+            if(countMax<count) countMax=count;
+            previousNumber=num;
+            num=scanner.nextInt();
+
+        }
+        System.out.println(countMax);
+    }
+}
